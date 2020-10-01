376cc01b30d92cf2ee1d37e9dfa15749a57392c6
Mayurg5899 <49452178+Mayurg5899@users.noreply.github.com>
2020-10-01 12:22:17 +0530

    Update __init__.py

--- gsheetsreader/__init__.py
+++ gsheetsreader/__init__.py
@@ -8,7 +8,8 @@ abspath2=os.path.join(path,"storage.json.json")#edit this also same
 sheets = Sheets.from_files(abspath1,abspath2)
 url="https://docs.google.com/spreadsheets/d/1SrZfvr2ee54r7HR1jGtAE9zHIj_Y-UzK9ok8bdwkpqc/edit#gid=0"#url for your current spreadsheets that you want to work with
 s=sheets.get(url)
-s.sheets[0].to_csv('Spam.csv', encoding='utf-8', dialect='excel')#converting that sheets into csv file for local manipulation
+sheet_no=int(input())#sheet no you want to get
+s.sheets[sheet_no].to_csv('Spam.csv', encoding='utf-8', dialect='excel')#converting that sheets into csv file for local manipulation
 df=pd.read_csv('Spam.csv')
 ax = plt.gca()
 
