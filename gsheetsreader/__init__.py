771916fd2fcd86d8247faf31cf0c693f0689147e
Mayurg5899 <49452178+Mayurg5899@users.noreply.github.com>
2020-10-01 12:24:25 +0530

    Update __init__.py

--- gsheetsreader/__init__.py
+++ gsheetsreader/__init__.py
@@ -8,7 +8,7 @@ abspath2=os.path.join(path,"storage.json.json")#edit this also same
 sheets = Sheets.from_files(abspath1,abspath2)
 url="https://docs.google.com/spreadsheets/d/1SrZfvr2ee54r7HR1jGtAE9zHIj_Y-UzK9ok8bdwkpqc/edit#gid=0"#url for your current spreadsheets that you want to work with
 s=sheets.get(url)
-sheet_no=int(input())#sheet no you want to get
+sheet_no=int(input())#sheet no you want to get ex:0,1in this case i have used 0 because i have one sheet only present
 s.sheets[sheet_no].to_csv('Spam.csv', encoding='utf-8', dialect='excel')#converting that sheets into csv file for local manipulation
 df=pd.read_csv('Spam.csv')
 ax = plt.gca()
