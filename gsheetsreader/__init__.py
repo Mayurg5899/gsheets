bfcb63d269c10e638dd8acb873a07c67ebcd9a8d
Mayurg5899 <49452178+Mayurg5899@users.noreply.github.com>
2020-10-01 12:02:05 +0530

    Update __init__.py

--- gsheetsreader/__init__.py
+++ gsheetsreader/__init__.py
@@ -6,9 +6,9 @@ path=os.getcwd()
 abspath1=os.path.join(path,"client_secrets.json") #edit your client_secret_json here with the the help of googleapis and entering into google developer console
 abspath2=os.path.join(path,"storage.json.json")#edit this also same 
 sheets = Sheets.from_files(abspath1,abspath2)
-url="https://docs.google.com/spreadsheets/d/1SrZfvr2ee54r7HR1jGtAE9zHIj_Y-UzK9ok8bdwkpqc/edit#gid=0"
+url="https://docs.google.com/spreadsheets/d/1SrZfvr2ee54r7HR1jGtAE9zHIj_Y-UzK9ok8bdwkpqc/edit#gid=0"#url for your current spreadsheets that you want to work with
 s=sheets.get(url)
-s.sheets[0].to_csv('Spam.csv', encoding='utf-8', dialect='excel')
+s.sheets[0].to_csv('Spam.csv', encoding='utf-8', dialect='excel')#converting that sheets into csv file for local manipulation
 df=pd.read_csv('Spam.csv')
 ax = plt.gca()
 
